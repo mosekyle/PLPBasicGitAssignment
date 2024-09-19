16c13bdd90a723281f58ac30a411a45d1a28cc94
Moses Gitau <112090830+mosekyle@users.noreply.github.com>
2024-09-19 08:46:45 +0300

    Program that asks user's name, age and location

---
+++ user_input.py
@@ -0,0 +1,40 @@
+# Create a new file "user_input.py" and write the following code
+# Asking the user for their name
+name = input("What is your name? ")
+
+# Asking the user for their age
+age = input("How old are you? ")
+
+# Asking the user for their location
+location = input("Where do you live? ")
+
+# Printing a personalized message using f-sring
+print(f"Hello {name}, you are {age} years old and live in {location}.")
+
+"""
+# Asking the user for their name
+name = input("What is your name? ")
+
+# Asking the user for their age
+age = input("How old are you? ")
+
+# Asking the user for their location
+location = input("Where do you live? ")
+
+# Printing a personalized message using string concatenation
+print("Hello " + name + ", you are " + age + " years old and live in " + location + ".")
+
+
+# Asking the user for their name
+name = input("What is your name? ")
+
+# Asking the user for their age
+age = input("How old are you? ")
+
+# Asking the user for their location
+location = input("Where do you live? ")
+
+# Printing a personalized message using the format method
+print("Hello {}, you are {} years old and live in {}.".format(name, age, location))
+
+"""
